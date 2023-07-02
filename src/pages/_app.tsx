5a39e6325f4f98c49ce2d60e6ae7c7949554c46c
ibqn <evgen.ibqn@gmail.com>
2023-07-02 10:27:33 +0200

    remove amplify and update to next 13

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import 'styles/globals.css'
+import '@/styles/globals.css'
 import '@fontsource/ibm-plex-sans'
 
 function MyApp({ Component, pageProps }) {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 
 export default function Home() {
   return (
@@ -10,7 +9,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="bg-teal-200 h-screen">
+      <main className="h-screen bg-teal-200">
         <h1 className="text-3xl font-bold underline">Hello world!</h1>
       </main>
 
