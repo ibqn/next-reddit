1950a01ff18da2916c73501ca60ff6b5c4b24c95
ibqn <evgen.ibqn@gmail.com>
2023-07-08 21:42:58 +0200

    get auth session

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,10 +1,13 @@
 import { Icon } from './icon'
 import Link from 'next/link'
 import { SignInButton } from './sign-in-button'
+import { getAuthSession } from '@/lib/auth'
 
 type Props = {}
 
 export const Navbar = async (props: Props) => {
+  const session = await getAuthSession()
+
   return (
     <div className="fixed inset-x-0 top-0 z-[10] h-fit border-b border-zinc-300 bg-zinc-100 py-2">
       <div className="container mx-auto flex h-full max-w-7xl items-center justify-between gap-2">
