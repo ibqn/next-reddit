dc5b9139c43ddd177e13c397d1e8787ae02d52e9
ibqn <evgen.ibqn@gmail.com>
2023-07-08 18:16:54 +0200

    working on create subreddit form

--- src/app/r/create/page.tsx
+++ src/app/r/create/page.tsx
@@ -2,16 +2,37 @@
 
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
+import { type CreateSubredditPayload, SubredditValidator } from '@/lib/validators'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useMutation } from '@tanstack/react-query'
 import { useRouter } from 'next/navigation'
+import { useForm } from 'react-hook-form'
+import axios from 'axios'
 
 type Props = {}
 
 export default function CreatePage({}: Props) {
   const router = useRouter()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<CreateSubredditPayload>({ resolver: zodResolver(SubredditValidator) })
+
+  const { mutate: createCommunity, isLoading } = useMutation({
+    mutationFn: (payload: CreateSubredditPayload) => axios.post('/api/subreddit', payload),
+  })
+
+  const onSubmit = handleSubmit((data) => {
+    try {
+      console.log(data)
+      createCommunity(data)
+    } catch (error) {}
+  })
 
   return (
     <div className="container mx-auto flex h-full max-w-3xl items-center">
-      <div className="relative h-fit w-full space-y-6 rounded-lg bg-white p-4">
+      <form onSubmit={onSubmit} className="relative h-fit w-full space-y-6 rounded-lg bg-white p-4">
         <div className="flex items-center justify-between">
           <h1 className="text-xl font-semibold">Create a Community</h1>
         </div>
@@ -23,27 +44,20 @@ export default function CreatePage({}: Props) {
           <p className="pb-2 text-xs">Community names including capitalization cannot be changed.</p>
           <div className="relative">
             <p className="absolute inset-y-0 left-0 grid w-8 place-items-center text-sm text-zinc-400">r/</p>
-            <Input className="pl-6" />
+            <Input className="pl-6" {...register('name')} />
           </div>
+          {errors.name && <div className="mt-2 text-sm text-red-400">{errors.name.message}</div>}
         </div>
 
         <div className="flex justify-end gap-4">
-          <Button
-            // disabled={isLoading}
-            variant="subtle"
-            onClick={() => router.back()}
-          >
+          <Button disabled={isLoading} variant="subtle" onClick={() => router.back()}>
             Cancel
           </Button>
-          <Button
-          // isLoading={isLoading}
-          // disabled={input.length === 0}
-          // onClick={createCommunity}
-          >
+          <Button type="submit" disabled={isLoading}>
             Create Community
           </Button>
         </div>
-      </div>
+      </form>
     </div>
   )
 }
