a61abeb85dc36bf1e81b3f1ce11f6aeb30647191
ibqn <evgen.ibqn@gmail.com>
2023-07-09 11:31:39 +0200

    working on subreddit post

---
+++ src/app/r/[subreddit]/page.tsx
@@ -0,0 +1,42 @@
+import { MiniCreatePost } from '@/components/mini-create-post'
+import { getAuthSession } from '@/lib/auth'
+import { prisma } from '@/lib/prisma'
+import { notFound } from 'next/navigation'
+import React from 'react'
+
+type Props = {
+  params: {
+    subreddit: string
+  }
+}
+
+export default async function SubredditPage({ params }: Props) {
+  const { subreddit: name } = params
+  const session = await getAuthSession()
+
+  const subreddit = await prisma.subreddit.findUnique({
+    where: { name },
+    include: {
+      posts: {
+        include: {
+          author: true,
+          votes: true,
+          comments: true,
+          subreddit: true,
+        },
+        orderBy: { createdAt: 'desc' },
+      },
+    },
+  })
+
+  if (!subreddit) {
+    return notFound()
+  }
+
+  return (
+    <>
+      <h1 className="h-14 text-3xl font-bold md:text-4xl">r/{subreddit.name}</h1>
+      <MiniCreatePost session={session} />
+    </>
+  )
+}
