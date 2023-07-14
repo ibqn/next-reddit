de5de03e81ca783ce31846dc0008a46a9d2bf7d8
Evgeny Bobkin <evgeny.bobkin@europace.de>
2023-07-14 22:46:57 +0200

    working on editor comp

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -1,28 +1,44 @@
 'use client'
 
-import TextareaAutosize from 'react-textarea-autosize'
+import EditorJS from '@editorjs/editorjs'
+import { useEffect, useRef, useState } from 'react'
+import { editorTools } from './editor-tools'
 
 type Props = {
-  subredditId: string
+  onReady?: () => void
 }
 
 export const Editor = (props: Props) => {
-  const onSubmit = () => {}
+  const editorJs = useRef<EditorJS>()
 
-  return (
-    <div className="w-full rounded-lg border border-zinc-200 bg-zinc-50 p-4">
-      <form id="subreddit-post-form" className="w-fit" onSubmit={onSubmit}>
-        <div className="prose prose-stone dark:prose-invert">
-          <TextareaAutosize
-            placeholder="Title"
-            className="w-full resize-none appearance-none overflow-hidden bg-transparent text-5xl font-bold focus:outline-none"
-          />
-          <div id="editor" className="min-h-[500px]" />
-          <p className="text-sm text-gray-500">
-            Use <kbd className="rounded-md border bg-muted px-1 text-xs uppercase">Tab</kbd> to open the command menu.
-          </p>
-        </div>
-      </form>
-    </div>
-  )
+  const [isMounted, setIsMounted] = useState(false)
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setIsMounted(true)
+    }
+  }, [])
+
+  useEffect(() => {
+    if (isMounted && !editorJs.current) {
+      const editor = new EditorJS({
+        holder: 'editor',
+        onReady() {
+          editorJs.current = editor
+          props?.onReady?.()
+        },
+        placeholder: 'Type here to write your post...',
+        inlineToolbar: true,
+        data: { blocks: [] },
+        tools: editorTools,
+      })
+    }
+
+    return () => {
+      editorJs.current?.destroy()
+      editorJs.current = undefined
+    }
+  }, [isMounted, props])
+
+  return <div id="editor" className="min-h-[500px]"></div>
 }
