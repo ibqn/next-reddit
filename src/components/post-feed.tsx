05b2fcbc28043603f91518c720eac90fd22b5f17
Evgeny Bobkin <evgeny.bobkin@europace.de>
2023-07-22 09:31:41 +0200

    working on post feed

--- src/components/post-feed.tsx
+++ src/components/post-feed.tsx
@@ -1,5 +1,23 @@
-type Props = {}
+import { Comment, Post, User, Vote } from '@prisma/client'
+import { PostItem } from './post-item'
+
+type Props = {
+  initialPosts: (Post & {
+    author: User
+    comments: Comment[]
+    votes: Vote[]
+  })[]
+  subredditName: string
+}
 
 export const PostFeed = (props: Props) => {
-  return <div>PostFeed</div>
+  const { initialPosts, subredditName } = props
+
+  return (
+    <div className="col-span-2 flex flex-col gap-6">
+      {initialPosts.map((post, index) => {
+        return <PostItem key={post.id} post={post} subredditName={subredditName} />
+      })}
+    </div>
+  )
 }
