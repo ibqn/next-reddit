19fdf08fe865fb2d967447581de552bce7b66b1b
Evgeny Bobkin <evgeny.bobkin@europace.de>
2023-07-22 10:59:16 +0200

    wrap dropdown menu in client only comp

--- src/components/user-account-nav.tsx
+++ src/components/user-account-nav.tsx
@@ -8,10 +8,12 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
+  DropdownMenuPortal,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from './ui/dropdown-menu'
+} from '@/components/ui/dropdown-menu'
 import { UserAvatar } from '@/components/user-avatar'
+import { ClientOnly } from './client-only'
 
 type Props = {
   user: User
@@ -19,42 +21,46 @@ type Props = {
 
 export function UserAccountNav({ user }: Props) {
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger>
-        <UserAvatar user={user} className="h-8 w-8" />
-      </DropdownMenuTrigger>
-      <DropdownMenuContent className="bg-white" align="end">
-        <div className="flex items-center justify-start gap-2 p-2">
-          <div className="flex flex-col space-y-1 leading-none">
-            {user.name && <p className="font-medium">{user.name}</p>}
-            {user.email && <p className="w-[200px] truncate text-sm text-muted-foreground">{user.email}</p>}
-          </div>
-        </div>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem asChild>
-          <Link href="/">Feed</Link>
-        </DropdownMenuItem>
+    <ClientOnly fallback={<div className="h-8 w-8 rounded-full bg-slate-200"></div>}>
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <UserAvatar user={user} className="h-8 w-8" />
+        </DropdownMenuTrigger>
+        <DropdownMenuPortal>
+          <DropdownMenuContent className="bg-white" align="end">
+            <div className="flex items-center justify-start gap-2 p-2">
+              <div className="flex flex-col space-y-1 leading-none">
+                {user.name && <p className="font-medium">{user.name}</p>}
+                {user.email && <p className="w-[200px] truncate text-sm text-muted-foreground">{user.email}</p>}
+              </div>
+            </div>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem asChild>
+              <Link href="/">Feed</Link>
+            </DropdownMenuItem>
 
-        <DropdownMenuItem asChild>
-          <Link href="/r/create">Create Community</Link>
-        </DropdownMenuItem>
+            <DropdownMenuItem asChild>
+              <Link href="/r/create">Create Community</Link>
+            </DropdownMenuItem>
 
-        <DropdownMenuItem asChild>
-          <Link href="/settings">Settings</Link>
-        </DropdownMenuItem>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem
-          className="cursor-pointer"
-          onSelect={(event) => {
-            event.preventDefault()
-            signOut({
-              callbackUrl: `${window.location.origin}/sign-in`,
-            })
-          }}
-        >
-          Sign out
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+            <DropdownMenuItem asChild>
+              <Link href="/settings">Settings</Link>
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem
+              className="cursor-pointer"
+              onSelect={(event) => {
+                event.preventDefault()
+                signOut({
+                  callbackUrl: `${window.location.origin}/sign-in`,
+                })
+              }}
+            >
+              Sign out
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenuPortal>
+      </DropdownMenu>
+    </ClientOnly>
   )
 }
