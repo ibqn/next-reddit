ac91d9ad2a29793e614cc68a6453b2d42463a8f9
Evgeny Bobkin <evgeny.bobkin@europace.de>
2023-07-24 18:58:57 +0200

    improve vote validator

--- src/lib/validators/vote.ts
+++ src/lib/validators/vote.ts
@@ -2,13 +2,13 @@ import { VoteType } from '@prisma/client'
 import { z } from 'zod'
 
 export const postVoteValidator = z.object({
-  voteType: z.enum([VoteType.UP, VoteType.DOWN]),
+  voteType: z.nativeEnum(VoteType, { required_error: 'Vote should be either UP or DOWN' }),
 })
 
 export type PostVotePayload = z.infer<typeof postVoteValidator>
 
 export const commentVoteValidator = z.object({
-  voteType: z.enum([VoteType.UP, VoteType.DOWN]),
+  voteType: z.nativeEnum(VoteType, { required_error: 'Vote should be either UP or DOWN' }),
 })
 
 export type CommentVotePayload = z.infer<typeof commentVoteValidator>
